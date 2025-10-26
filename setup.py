3c0bbb9ca212c19582322b82967d776a0124a496
Soohaeng Yoo Willow <sy7willow@gmail.com>
2025-10-26 19:46:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,17 +11,19 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        #Operating System :: OS Independent",
+        "Operating System :: OS Independent",
     ],
     install_requires=[
-        "optax",
-        "flax",
-        "ase",
-        "matscipy",
-        "jraph",
-        "e3nn-jax",
-        "dm-haiku",
-        "pennylane"
+        "pennylane==0.42.3",
+        "jax==0.6.2",
+        "jaxlib==0.6.2",
+        "matscipy==1.1.1",
+        "ase==3.23.0",
+        "optax==0.2.5",
+        "flax==0.11.2",
+        "jraph==0.0.6.dev0",
+        "e3nn-jax==0.20.8",
+        "dm-haiku==0.0.14"
     ],
 )
 
